f0215ae96fa6f8c685394c13390b3e5c67fe2c15
gderuwez <gderuwez@gmail.com>
2018-05-15 13:07:41 +0200

    using grid and flex

--- curriculumVitae/index.php
+++ curriculumVitae/index.php
@@ -6,17 +6,22 @@
       <div class="flexy">
         <div class="asideLeft">
           <img id="avatar" src="./assets/images/avatar.png" alt="Portrait de guillaume">
+          <div class="line"></div>
           <h3 class="Name">Name</h3>
           <p class="NameR">Guillaume Deruwez</p>
           <h3 class="Title">Title</h3>
           <p class="TitleL">Web-developper</p>
+          <div class="line2"></div>
           <h3 class="background">Background</h3>
-          <i class="fab fa-git"></i>
-          <p class="git">github</p>
-          <i class="fab fa-html5"></i>
-          <p class="code">html</p>
-          <i class="fab fa-css3-alt"></i>
-          <p class="style">css</p>
+          <div class="subGrid">
+            <i class="fab fa-git"></i>
+            <p class="git">github</p>
+            <i class="fab fa-html5"></i>
+            <p class="code">html</p>
+            <i class="fab fa-css3-alt"></i>
+            <p class="style">css</p>
+          </div>
+          <div class="line3"></div>
           <h3 class="tech">Tech preferences</h3>
           <p class="pre">
             exemple 1<br>
@@ -25,16 +30,24 @@
           </p>
         </div>
         <div class="main">
-          <h2>Ce que je fais</h2>
-          <p>En formation <a href="https://www.linkedin.com/in/guillaume-deruwez-98328a134/" title="lien linkedin" target="_blank">@Becode</a></p>
-          <h2>Ce que j'aimerais faire</h2>
-          <p>Je voudrais devenir web-developper fullstack</p>
-          <h2>Mes hobbys</h2>
-          <ul>
-            <li>Lecture</li>
-            <li>Jeux de rôles</li>
-            <li>Musique</li>
-          </ul>
+          <div>
+            <h2>Ce que je fais</h2>
+            <p>En formation <a href="https://www.linkedin.com/in/guillaume-deruwez-98328a134/" title="lien linkedin" target="_blank">@Becode</a></p>
+          </div>
+          <div class="line3"></div>
+          <div>
+            <h2>Ce que j'aimerais faire</h2>
+            <p>Je voudrais devenir web-developper fullstack</p>
+          </div>
+          <div class="line4"></div>
+          <div>
+            <h2>Mes hobbys</h2>
+            <ul>
+              <li>Lecture</li>
+              <li>Jeux de rôles</li>
+              <li>Musique</li>
+            </ul>
+        </div>
         </div>
       </div>
     </section>
